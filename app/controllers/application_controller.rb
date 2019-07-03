b46cee51cf1765c8580337b463b0e977c3df3caf
Travis Exline <t.exline22@gmail.com>
2019-07-03 15:33:12 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,4 +15,14 @@ class ApplicationController < Sinatra::Base
 
     erb :new
   end
+
+  post '/articles' do
+    @article = Article.create(params)
+    redirect to "/articles/#{ @article.id }"
+  end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
 end
